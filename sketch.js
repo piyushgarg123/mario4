08a31968ca08355befd2b9b1407789dc029c38b0
piyushgarg123 <87375523+piyushgarg123@users.noreply.github.com>
2021-08-06 18:32:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,74 +1,127 @@
-var grid = 50;
-var width = 1366;
-var carGroup1,logGroup1;
-var grassHeight = 100;
-var gameState = "play";
-var carAnimation, logAnimation, playerAnimation;
-var school;
-var player3;
+var mario;
+var platformGroup, obstacleGroup;
+var marioAnimation, obstacleAnimation, wallAnimation, groundAnimation;
+var flag;
+var LOSE=0;
+var PLAY=1;
+var WIN=2;
+var gameState=PLAY;
 function preload()
 {
- 
+  marioAnimation=loadAnimation("images/Capture1.png","images/Capture4.png","images/Capture3.png");
+  obstacleAnimation=loadAnimation("images/obstacle1.png");
+  wallAnimation=loadAnimation("images/wall.png");
+  groundAnimation=loadAnimation("images/ground.png");  
+  flagAnimation=loadAnimation("images/Flag.png");
 }
 
 function setup() {
-  createCanvas(1366,2700);
-
-  //creating grasses where player can rest
-  for(var i=0;i<6;i++){
-    var bottomgrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
-    bottomgrass1.shapeColor="grey";
-    if(i%2===0)  //adding road
-    {
-      var road = createSprite(683,height-150-(i*400)-grassHeight,width,300)
-      road.shapeColor="black";
-      
-    }
+  //Creating canvas equal to width and height of display
+  createCanvas(displayWidth,668);
+  var countDistanceX = 0;
+  var platform;
+  var gap;
+  
+  //creating a player mario
+  mario = new Player();
+  
+  //creating a group
+  platformGroup= createGroup();
+  obstacleGroup=createGroup();
+  //adding platforms to stand for mario
+  for (var i=0;i<26;i++)
+	 {
+     frameRate(30);
+      platform = new Platform(countDistanceX);
+      platformGroup.add(platform.spt);//Adding each new platform to platformGroup
+      gap=random([0,0,0,0,200]);//givin randome value to gap
+      countDistanceX = countDistanceX + platform.spt.width + gap; //counting x location of next platform to be build
+      //adding wall to the game
+      if(i%3===0)
+      {
+      wall=new Wall(countDistanceX);
+      platformGroup.add(wall.spt);
+      }
+      //adding obstacles to the game
+      if(i%4==0)
+      {
+      obstacle=new Obstacle(countDistanceX);
+      obstacleGroup.add(obstacle.spt);
+      }
   }
-  var player = createSprite ()
-  carGroup1 = new Group();
-  logGroup1 = new Group();
-  //create rows of car
-  for(var i=0;i<40;i++){
-    cars=new Car(2);
-    carGroup1.add(cars.spt);
-
-  }
-  //create rows of log
-  for(var i=0;i<40;i++){
-    logs=new Log(-2);
-    logGroup1.add(logs.spt);
-
-  }
- 
-   
- }
+  flag=createSprite(countDistanceX-150,height-320);
+  flag.addAnimation("flagimg",flagAnimation);
+  flag.scale=0.09;
+  flag.setCollider("rectangle",0,0,1100,6520);
+}
 
 function draw() {
-  background("skyblue");
- translate(0,-player.spt + height-150);
- 
- //Making the cars re-apper
- for(i=1;i<carGroup1.length;i++) {
-  if(carGroup1[i].x>width)
-  {
-   carGroup1[i].x=0;
-  }
-  if(carGroup1[i].x<0)
-  {
-    carGroup1[i].x=width;
-  }
+  background('skyblue');
+  //code to move the camera
+  translate(  -mario.spt.x + width/2 , 0);
+  if(gameState==PLAY)//Play state
+  {  
+          //apply gravity to mario and set colliding with platforms
+        mario.applyGravity();
+        mario.spt.collide(platformGroup);
+        
+        //Calling various function to controll mario
+        if (keyDown("left"))  
+        { 
+          mario.moveLeft();
+        }
+        if (keyDown("right")) 
+        { 
+          mario.moveRight();
+        }
+        
+        
+        
+        if (keyDown("up") && mario.spt.velocityY===0) 
+        {
+          mario.jump();
+        }
+        if(obstacleGroup.isTouching(mario.spt)||mario.spt.y>height ){
+
+          gameState = LOSE;
+        }
+
+   if(flag.isTouching(mario.spt)){
+     gameState=WIN   
+    }
+
+
+
+   }
+
+  if(gameState==LOSE)//END State
+  {  
+
+mario.spt.setVelocity(0,0);
+mario.spt.pause();
+ obstacleGroup.destroyEach(); 
+textSize(100);
+fill("blue");
+ text("Game Over",mario.spt.x,300);
+
 }
 
-//making the logs reapper
-for(i=1;i<logGroup1.length;i++){
-  if(logGroup1[i].x<0)
-  {
-  logGroup1[i].x=width;
+  if(gameState==WIN)//WIN state
+  {  
+
+
+    mario.spt.setVelocity(0,0);
+    mario.spt.pause();
+     obstacleGroup.destroyEach(); 
+    textSize(100);
+    fill("pink");
+    text("WINNER",mario.spt.x,300);
+    
   }
+  
+
+   drawSprites();
 }
 
-  drawSprites();
 
-}
 
